6f3b7cff2f962fbf460d53f8e38761a4c98e3809
seyfu-t <seyfullahtalay@hotmail.com>
2024-09-02 02:32:36 +0200

    basic maven structure added

---
+++ src/main/java/me/seyfu-t/JBarcode_D1/App.java
@@ -0,0 +1,13 @@
+package me.seyfu-t.JBarcode_D1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
